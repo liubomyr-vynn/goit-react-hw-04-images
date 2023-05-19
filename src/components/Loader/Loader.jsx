66c72670b88cf5345e16bfc8d309e7135d07930f
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-05-19 13:54:55 +0300

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,22 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { ThreeDots } from 'react-loader-spinner';
 
-class Loader extends Component {
-  render() {
-    return (
-      <div className="loader">
-        <ThreeDots
-          height="80"
-          width="80"
-          radius="9"
-          color="#4fa94d"
-          ariaLabel="three-dots-loading"
-          wrapperStyle={{}}
-          wrapperClassName=""
-          visible={true}
-        />
-      </div>
-    );
-  }
-}
+const Loader = () => {
+  return (
+    <div className="loader">
+      <ThreeDots
+        height="80"
+        width="80"
+        radius="9"
+        color="#4fa94d"
+        ariaLabel="three-dots-loading"
+        visible={true}
+      />
+    </div>
+  );
+};
+
 export default Loader;
