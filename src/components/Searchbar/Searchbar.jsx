807f2c4433c99f44237fe4e2be5c5a6926d7967f
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-05-19 13:51:41 +0300

    Update useState

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,50 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { ImSearch } from 'react-icons/im';
 import { Report } from 'notiflix/build/notiflix-report-aio';
 
-class Searchbar extends Component {
-  state = {
-    inputValue: '',
+const Searchbar = ({ onFormSubmit }) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInput = event => {
+    setInputValue(event.target.value);
   };
 
-  handleInput = event => {
-    this.setState({ inputValue: event.target.value });
-  };
-
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    if (this.state.inputValue === '') {
+    if (inputValue === '') {
       Report.info('Enter a search query!', '', 'Ok');
     }
-    this.props.onFormSubmit(this.state.inputValue);
-    this.setState({ inputValue: '' });
+    onFormSubmit(inputValue);
+    setInputValue('');
   };
 
-  render() {
-    const { inputValue } = this.state;
-
-    return (
-      <div className="Searchbar">
-        <form onSubmit={this.handleSubmit} className="SearchForm">
-          <label className="SearchForm-button-label" htmlFor="id-1"></label>
-          <input
-            onChange={this.handleInput}
-            value={inputValue}
-            type="text"
-            placeholder="Search images"
-            autoComplete="off"
-            autoFocus
-            name="search"
-            className="SearchForm-input"
-            id="id-1"
-          />
-          <button type="submit" className="SearchForm-button">
-            <ImSearch style={{ width: 18, height: 18 }} />
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="Searchbar">
+      <form onSubmit={handleSubmit} className="SearchForm">
+        <label className="SearchForm-button-label" htmlFor="id-1"></label>
+        <input
+          onChange={handleInput}
+          value={inputValue}
+          type="text"
+          placeholder="Search images"
+          autoComplete="off"
+          autoFocus
+          name="search"
+          className="SearchForm-input"
+          id="id-1"
+        />
+        <button type="submit" className="SearchForm-button">
+          <ImSearch style={{ width: 18, height: 18 }} />
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Searchbar;
