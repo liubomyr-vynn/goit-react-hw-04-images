5852d05ac969ebc34e6db66341cc725019f5028b
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-05-20 16:30:04 +0300

    Update Gallery-api.js

--- src/Services/Gallery-api.js
+++ src/Services/Gallery-api.js
@@ -1,19 +1,13 @@
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '34845172-53e67b2f5b8cb8ccf3124ff1b';
 
-const fetchImages = (query, page) => {
-  return fetch(
+const fetchImages = async (query, page) => {
+  const response = await fetch(
     `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=12
-
-`
-  ).then(response => {
-    if (response.ok) {
-      return response.json();
-    }
-    return Promise.reject(
-      new Error(`Oops... there are no images matching your search... `)
-    );
-  });
+  `
+  );
+  const images = await response.json();
+  return images;
 };
 
 const api = {
