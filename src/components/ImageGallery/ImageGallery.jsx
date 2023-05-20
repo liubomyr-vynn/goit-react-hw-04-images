70f411a2e3bcc2ed4b1027882b30cbf89e56558a
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-05-20 16:30:49 +0300

    Update useState, useEffect

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 import Button from '../Button/Button';
 import imagesApi from '../../Services/Gallery-api';
@@ -6,93 +6,87 @@ import Loader from '../Loader/Loader';
 import { Report } from 'notiflix/build/notiflix-report-aio';
 import { nanoid } from 'nanoid';
 
-class ImageGallery extends Component {
-  state = {
-    searchQuery: '',
-    currentPage: 1,
-    images: [],
-    loading: false,
-    totalHits: 0,
-  };
+const ImageGallery = ({ inputValue }) => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [totalHits, setTotalHits] = useState(0);
 
-  componentDidUpdate(prevProps, prevState) {
-    const prevInput = prevProps.inputValue;
-    const nextInput = this.props.inputValue;
-    const prevStatePage = prevState.currentPage;
-    const nextStatePage = this.state.currentPage;
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
 
-    if (prevInput !== nextInput) {
-      this.setState({ loading: true });
+      try {
+        const data = await imagesApi.fetchImages(inputValue, 1);
+        const newImages = data.hits;
+        if (newImages.length === 0) {
+          throw new Error('No photos found for this query');
+        }
+        setSearchQuery(inputValue);
+        setCurrentPage(1);
+        setImages(newImages);
+        setTotalHits(data.totalHits);
+      } catch (error) {
+        Report.info('No photos found for this query!', '', 'Ok');
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-      imagesApi
-        .fetchImages(nextInput, 1)
-        .then(data => {
-          const newImages = data.hits;
-          if (newImages.length === 0) {
-            throw new Error('No photos found for this query');
-          }
-          this.setState({
-            searchQuery: nextInput,
-            currentPage: 1,
-            images: newImages,
-            totalHits: data.totalHits,
-          });
-        })
-        .catch(error => {
-          Report.info('No photos found for this query!', '', 'Ok');
-          console.log(error);
-        })
-        .finally(() => {
-          this.setState({ loading: false });
-        });
-    } else if (prevStatePage !== nextStatePage) {
-      this.setState({ loading: true });
-
-      imagesApi
-        .fetchImages(nextInput, nextStatePage)
-        .then(data => {
-          const newImages = data.hits;
-          this.setState(prevState => ({
-            images: [...prevState.images, ...newImages],
-          }));
-        })
-        .catch(error => {
-          Report.info('No photos found for this query!', '', 'Ok');
-          console.log(error);
-        })
-        .finally(() => {
-          this.setState({ loading: false });
-        });
+    if (inputValue !== '') {
+      fetchData();
     }
-  }
+  }, [inputValue]);
 
-  handleLoadMore = () => {
-    this.setState(prevState => ({
-      currentPage: prevState.currentPage + 1,
-    }));
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+
+      try {
+        const data = await imagesApi.fetchImages(inputValue, currentPage);
+        const newImages = data.hits;
+        setImages(prevImages => [...prevImages, ...newImages]);
+      } catch (error) {
+        Report.info('No photos found for this query!', '', 'Ok');
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (currentPage > 1) {
+      fetchData();
+    }
+  }, [currentPage]);
+
+  useEffect(() => {
+    setImages([]);
+  }, [inputValue]);
+
+  const handleLoadMore = () => {
+    setCurrentPage(prevPage => prevPage + 1);
   };
 
-  render() {
-    const { searchQuery, images, loading, totalHits } = this.state;
-    const shouldRenderLoadMore = images.length < totalHits;
+  const shouldRenderLoadMore = images.length < totalHits;
 
-    return (
-      <div>
-        <ul className="ImageGallery">
-          {searchQuery !== '' &&
-            images.map(image => (
-              <ImageGalleryItem
-                key={nanoid()}
-                largeImage={image.largeImageURL}
-                webformat={image.webformatURL}
-              />
-            ))}
-        </ul>
-        {loading && <Loader />}
-        {shouldRenderLoadMore && <Button onChange={this.handleLoadMore} />}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <ul className="ImageGallery">
+        {searchQuery !== '' &&
+          images.map(image => (
+            <ImageGalleryItem
+              key={nanoid()}
+              largeImage={image.largeImageURL}
+              webformat={image.webformatURL}
+            />
+          ))}
+      </ul>
+      {loading && <Loader />}
+      {shouldRenderLoadMore && <Button onChange={handleLoadMore} />}
+    </div>
+  );
+};
 
 export default ImageGallery;
